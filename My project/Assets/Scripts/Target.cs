8deaab9d23ef8200a4039cdb647e1a1e757e2b82
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2022-11-09 11:04:13 -0800

    Added Cursor

--- My project/Assets/Scripts/Target.cs
+++ My project/Assets/Scripts/Target.cs
@@ -7,19 +7,29 @@ public class Target : MonoBehaviour
 
     public int pointValue = 1;
     public float minSpeed = 10;
-    public float maxSpeed = 20;
+    public float maxSpeed = 50;
     public float MaxTorque = 10;
      private Rigidbody2D _targetRB;
     // Start is called before the first frame update
     void Start()
     {
         _targetRB = GetComponent<Rigidbody2D>();
-        _targetRB.AddForce(Vector2.up * minSpeed, ForceMode2D.Impulse);
+        _targetRB.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
+        _targetRB.AddTorque(RandomTorque());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private float RandomizeForce()
+    {
+        return Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-MaxTorque, MaxTorque);
     }
 }
